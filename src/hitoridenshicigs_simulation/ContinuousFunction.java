83fde7987073aa5795fb00b4a227fbc1abd2170a
audreyazura <audreyazura@localhost>
2020-04-23 01:06:01 +0900

    Adding proper constructor, from file. Making m_abscissa and m_values Set and Map

--- src/hitoridenshicigs_simulation/ContinuousFunction.java
+++ src/hitoridenshicigs_simulation/ContinuousFunction.java
@@ -21,11 +21,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Pattern;
 import java.util.zip.DataFormatException;
 
 /**
@@ -35,48 +36,44 @@ import java.util.zip.DataFormatException;
 class ContinuousFunction
 {
     //do not truncate values here: the field is also defined outside the absorber. Only the absorber knows if a particle exited itself. A ContinuousFunction can only say if a given position is in its range.
-    private final TreeSet<Double> m_abscissa;
-    private final HashMap<Double, Double> m_values;
+    private final Set<Double> m_abscissa;
+    private final Map<Double, Double> m_values;
     
-    public ContinuousFunction (File p_fileValues) throws DifferentArraySizeException, FileNotFoundException, DataFormatException, IOException, ArrayIndexOutOfBoundsException
+    static public ContinuousFunction createElectricField(File p_fileValues) throws DifferentArraySizeException, DataFormatException, IOException, ArrayIndexOutOfBoundsException
     {
+       return new ContinuousFunction(p_fileValues, "eb", 23, new int[] {1,12});
+    }
+    
+    private ContinuousFunction (File p_fileValues, String p_expectedExtension, int p_ncolumn, int[] columnToExtract) throws DifferentArraySizeException, FileNotFoundException, DataFormatException, ArrayIndexOutOfBoundsException, IOException
+    {
+        m_abscissa = new TreeSet<>();
+        m_values = new HashMap<>();
+        
         String[] nameSplit = p_fileValues.getName().split(".");
         
-        if (nameSplit[nameSplit.length-1].equals("gen"))
+        if (nameSplit[nameSplit.length-1].equals(p_expectedExtension))
         {
             throw new DataFormatException();
         }
         
-        List<Double> extractedAbscissa = new ArrayList<>();
-        List<Double> extractedValues = new ArrayList<>();
-        
         BufferedReader fieldFile = new BufferedReader(new FileReader(p_fileValues));
+        Pattern numberRegex = Pattern.compile("^\\-?\\d+(.\\d+(e(\\+|\\-)\\d+)?)?");
 	
 	String line;
 	while (((line = fieldFile.readLine()) != null))
 	{	    
 	    String[] cutSplit = line.strip().split("\t");
 	    
-	    if(cutSplit.length == 23)
+	    if(cutSplit.length == p_ncolumn && numberRegex.matcher(cutSplit[0]).matches())
 	    {
-		
+		double currentAbscissa = Double.valueOf(cutSplit[columnToExtract[0]]);
+                
+                if (!m_abscissa.contains(currentAbscissa))
+                {
+                    m_abscissa.add(currentAbscissa);
+                    m_values.put(currentAbscissa, Double.valueOf(cutSplit[columnToExtract[1]]));
+                }
 	    }
-        }
-        
-        if (extractedAbscissa.size() == extractedValues.size())
-        {            
-            m_abscissa = new TreeSet<>();
-            m_values = new HashMap<>();
-            
-            for (double currentAbscissa: extractedAbscissa)
-            {
-                m_abscissa.add(currentAbscissa);
-                m_values.put(currentAbscissa, extractedValues.get(extractedAbscissa.indexOf(currentAbscissa)));
-            }
-        }
-        else
-        {
-            throw new DifferentArraySizeException("The absissa and value table are of different sizes.", extractedAbscissa.size(), extractedValues.size());
         }
     }
     
@@ -89,12 +86,12 @@ class ContinuousFunction
     
     public TreeSet getAbscissa()
     {
-        return (TreeSet) m_abscissa.clone();
+        return new TreeSet(m_abscissa);
     }
     
     public HashMap getValues()
     {
-        return (HashMap) m_values.clone();
+        return new HashMap(m_values);
     }
             
     public double getValueAtPosition(double position)
@@ -109,8 +106,8 @@ class ContinuousFunction
             }
             else
             {
-                double previousPosition = m_abscissa.lower(position);
-                double nextPosition = m_abscissa.higher(position);
+                double previousPosition = ((TreeSet<Double>) m_abscissa).lower(position);
+                double nextPosition = ((TreeSet<Double>) m_abscissa).higher(position);
                 
                 double interpolationSlope = (m_values.get(nextPosition) - m_values.get(previousPosition)) / (nextPosition - previousPosition);
                 double interpolationOffset = m_values.get(previousPosition) - interpolationSlope*previousPosition;
@@ -128,6 +125,6 @@ class ContinuousFunction
     
     private boolean isInRange(double position)
     {
-        return position >= m_abscissa.first() && position <= m_abscissa.last();
+        return position >= ((TreeSet<Double>) m_abscissa).first() && position <= ((TreeSet<Double>) m_abscissa).last();
     }
 }

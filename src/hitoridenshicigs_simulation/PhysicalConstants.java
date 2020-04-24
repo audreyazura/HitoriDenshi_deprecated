030771828fa132690ed61f419ea5de100c0c7c9f
audreyazura <audreyazura@localhost>
2020-04-25 00:36:04 +0900

    Creating BigDecimal from String

--- src/hitoridenshicigs_simulation/PhysicalConstants.java
+++ src/hitoridenshicigs_simulation/PhysicalConstants.java
@@ -27,11 +27,11 @@ public class PhysicalConstants
     //USE BIGDECIMAL INSTEAD OF DOUBLE, OR SEARCH MORE PRECISE METHOD
     
     //Boltzman constant in J/K
-    static final public BigDecimal KB = new BigDecimal(1.380649e-23);
+    static final public BigDecimal KB = new BigDecimal("1.380649e-23");
     //electron mass in kg
-    static final public BigDecimal ME = new BigDecimal(9.10938188e-31);
+    static final public BigDecimal ME = new BigDecimal("9.10938188e-31");
     //elementary charge in C
-    static final public BigDecimal Q = new BigDecimal(1.60217733e-19);
+    static final public BigDecimal Q = new BigDecimal("1.60217733e-19");
     
     //contains the possible units multiplier
     static public enum UnitsPrefix

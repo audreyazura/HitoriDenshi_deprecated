706f86bc11ea048e2e3724e8cfed862301492ba4
audreyazura <audreyazura@localhost>
2020-05-16 21:04:15 +0900

    More general name for future purpose

---
+++ src/hitoridenshi/executionmanager/Launcher.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Alban Lafuente
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package hitoridenshi.executionmanager;
+
+import hitoridenshi.guimanager.GUIManager;
+
+/**
+ *
+ * @author Alban Lafuente
+ */
+public class Launcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        String[] arguments = {"--file=ConfigurationFiles/default.conf"};
+        LauncherGUIManager guiLaunch = new GUIManager();
+        guiLaunch.startGUI(arguments);
+    }
+    
+}
